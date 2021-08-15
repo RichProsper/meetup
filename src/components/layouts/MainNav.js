7c9c6075a133a9c6551d87f2a6c43a253c8f9f08
RichProsper <r.stp.p@hotmail.com>
2021-08-14 20:11:26 -0400

    Netlify setup

--- src/components/layouts/MainNav.js
+++ src/components/layouts/MainNav.js
@@ -1,23 +1,37 @@
 import { Link } from 'react-router-dom'
 import classes from './MainNav.module.css'
-import { useContext, useState } from 'react'
+import { useContext, useState, useRef } from 'react'
 import FavoritesContext from '../../store/FavoritesContext'
 
 export default function MainNav() {    
     const favsContext = useContext(FavoritesContext)
     const [toggle, setToggle] = useState(false)
+    const btn = useRef()
+    const bars = useRef()
+
+    /**
+     * @param {MouseEvent} e 
+     */
+    const closeToggle = e => {
+        if (e.target !== btn.current && e.target !== bars.current) setToggle(false)
+    }
 
     return (
-        <header className={classes.MainNav} onClick={() => {setToggle(!toggle)}}>
+        <header className={classes.MainNav} onClick={closeToggle}>
             <div className={classes.logo}>MeetUps</div>
             <nav>
-                <button type="button" className={classes.toggle}>
+                <button 
+                    type="button" 
+                    ref={btn}
+                    className={classes.toggle} 
+                    onClick={() => {setToggle(!toggle)}}
+                >
                     {toggle ? <div className={classes.times}>&times;</div> : (
-                        <div className={classes.bars}></div>
+                        <div ref={bars} className={classes.bars}></div>
                     )}
                 </button>
 
-                <ul className={toggle ? classes.visible : ''}>
+                <ul className={toggle ? classes.visible : ''} onClick={() => {setToggle(false)}}>
                     <li><Link to="/">All Meetups</Link></li>
                     <li><Link to="/new-meetup">New Meetup</Link></li>
                     <li>
